fa6b910b1679f44cfee6ac20a5cfcd84882b9749
Schlaback, Nils <nils.schlaback@stud.uni-heidelberg.de>
2023-03-09 18:31:14 +0100

    Changed Grid Line Color

--- src/grid/gridRenderer.js
+++ src/grid/gridRenderer.js
@@ -13,8 +13,9 @@ const GREEN = "#05c46b";
 const RED = "#ff3f34";
 const DARKBLUE = "#3c40c6";
 const LIGHTBLUE = "#575fcf";
+const GREY = "#485460";
 
-let cellsize = 25;
+let cellsize = 24;
 ctx.strokeStyle = WHITE;
 
 function renderGrid() {
@@ -22,13 +23,15 @@ function renderGrid() {
 	//Outline
 	ctx.strokeRect(1, 1, canvas.width - 2, canvas.height - 2);
 
-	//Cells
-	// for (let i = 0; i <= canvas.width; i += cellsize) {
-	// 	for (let j = 0; j <= canvas.height; j += cellsize) {
-	// 		ctx.strokeRect(i, j, cellsize, cellsize);
-	// 	}
-	// }
+	//Grid lines
+	ctx.strokeStyle = GREY;
+	for (let i = 0; i <= canvas.width; i += cellsize) {
+		for (let j = 0; j <= canvas.height; j += cellsize) {
+			ctx.strokeRect(i, j, cellsize, cellsize);
+		}
+	}
 
+	ctx.strokeStyle = WHITE;
 	for (let i = 0; i < 720 / cellsize; i++) {
 		for (let j = 0; j < 360 / cellsize; j++) {
 			switch (gridManager.gridWalls[i][j]) {
